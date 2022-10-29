0a71717778726570304ef2a2205e58c71990cde1
Lakshman <lakshman14334@gmail.com>
2022-10-28 22:41:50 -0400

    stage 4 prc and scores added

--- src/stage_04_evaluate.py
+++ src/stage_04_evaluate.py
@@ -35,7 +35,7 @@ def main(config_path, params_path):
     labels = np.squeeze(matrix[:, 1].toarray())
     X = matrix[:, 2:]
     predictions_by_class = model.predict_proba(X)
-    predictions = predictions_by_class[: 1]
+    predictions = predictions_by_class[:, 1]
 
     PRC_json_path = config['plots']['PRC']
     ROC_json_path = config['plots']['ROC']
